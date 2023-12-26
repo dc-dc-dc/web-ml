0856fd06341ceb197c710c30162820b66148c15c
dc-dc-dc <dgcruz983@gmail.com>
2023-12-26 13:27:07 -0500

    added error checks

--- src/main.ts
+++ src/main.ts
@@ -33,12 +33,21 @@ class Tensor {
   private _evaluated: boolean = false;
 
   public constructor({ shape, dtype, op, inputs, data }: TensorArgs) {
+    if(shape.length < 0 || shape.length > 2) {
+      throw new Error("Only 1D and 2D tensors are supported");
+    }
     this._dtype = dtype;
     this._shape = shape;
     if (op) {
       this._op = op;
       this._inputs = inputs;
     } else {
+      if (!data) {
+        throw new Error("Cannot create tensor without data or op");
+      }
+      if(data.length !== shape.reduce((a, b) => a * b, 1)) {
+        throw new Error(`Data length ${data.length} does not match shape ${shape}`);
+      }
       switch (dtype) {
         case "float32":
           this._data = new Float32Array(data);
@@ -154,25 +163,32 @@ class Tensor {
     this._evaluated = true;
   }
 
-  async list(): Promise<Array<number>> {
+  async list(): Promise<number[] | number[][]> {
     if (!this._evaluated) {
       await this.eval();
     }
-    return Array.from(this._data);
+    if(!this._data) {
+      throw new Error("Cannot list without data");
+    }
+    switch(this.shape.length) {
+    case 1:
+      return Array.from(this._data);
+    case 2:
+      const rows = this.shape.length > 1 ? this.shape[1] : this.shape[0];
+      let res = Array.from({length: this.shape[0]}, () => Array(rows).fill(0));
+      for(let i = 0; i < this._data.length; i++) {
+        res[Math.floor(i / rows)][i % rows] = this._data[i];
+      }
+      return res;
+    default:
+      throw new Error("Unsupported shape");
+    }
   }
 }
 
 console.time("creating");
 const a = new Tensor({ shape: [2, 2], dtype: float32, data: [1, 2, 3, 4] });
 const b = new Tensor({ shape: [2, 2], dtype: float32, data: [1, 2, 3, 4] });
-const c = a.add(b);
+const c = a.mul(b).add(b.add(a));
 const d = c.mul(c);
-console.timeEnd("creating");
-console.time("evaluating");
-// console.log(await c.list());
-console.timeEnd("evaluating");
-console.time("evaluating 2");
-console.log(await d.list());
-console.timeEnd("evaluating 2");
-console.log(await c.list());
 console.log(await d.list());
